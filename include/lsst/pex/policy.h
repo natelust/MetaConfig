5e1fa54a7bff3ac3b8aecade67ec90446fa271fd
=Martin Dubcovsky <dubcovsky@neon.physics.ucdavis.edu>
2011-12-09 17:38:43 -0800

    Deleted all the cruft from the old Policy implementation

--- include/lsst/pex/policy.h
+++ include/lsst/pex/policy.h
@@ -23,14 +23,4 @@
 #ifndef LSST_PEX_POLICY_H
 #define LSST_PEX_POLICY_H
 
-#include "lsst/pex/policy/exceptions.h"
-#include "lsst/pex/policy/parserexceptions.h"
-#include "lsst/pex/policy/Policy.h"
-#include "lsst/pex/policy/Dictionary.h"
-#include "lsst/pex/policy/PolicyWriter.h"
-#include "lsst/pex/policy/PolicyString.h"
-#include "lsst/pex/policy/DefaultPolicyFile.h"
-#include "lsst/pex/policy/paf/PAFWriter.h"
-#include "lsst/pex/policy/UrnPolicyFile.h"
-
 #endif
--- tests/test_validate.sh
+++
@@ -1,3 +0,0 @@
-#! /bin/sh
-# TODO: include something like this in unit tests
-python bin/validate.py -v -l foo -L tests/dictionary tests/dictionary/defaults_policy_more.paf tests/dictionary/defaults_dictionary_good.paf
