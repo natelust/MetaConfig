5a3924cd010cbd39b2e108b2e54da1da9a8234e0
Martin Dubcovsky <dubcovsky@neon.physics.ucdavis.edu>
2011-12-31 08:00:34 -0800

    All field types now pass tests

--- tests/config.py
+++ tests/config.py
@@ -27,6 +27,18 @@ from lsst.pex.policy import *
 import os
 
 
+class Simple(Config):
+    i = Field(int, "integer test", optional=True)
+    f = Field(float, "float test", default=3.0)
+    b = Field(bool, "boolean test", default=False, optional=False)
+    t = Field(tuple, "tuple test", default=("A", 1), optional=False, 
+            check=lambda x: len(x)==2 and type(x[0])==str and type(x[1])==int)
+    c = ChoiceField(str, "choice test", default="Hello",
+            allowed={"Hello":"First choice", "World":"second choice"})
+    r = RangeField(float, "Range test", default = 3.0, optional=False,
+            min=3.0, inclusiveMin=True)
+    l = ListField(int, "list test", default=[1,2,3], maxLength=5, itemCheck=lambda x: x is not None and x>0)
+
 
 class InnerConfig(Config):
     f = Field(float, "Inner.f", default=0.0, check = lambda x: x >= 0, optional=False)
@@ -43,80 +55,83 @@ class OuterConfig(InnerConfig, Config):
         if self.i.f < 5:
             raise ValueError("validation failed, outer.i.f must be greater than 5")
 
-class PluginConfig(Config):
-    algA = ConfigField(InnerConfig, "alg A policy", optional=True)
-    algB = ConfigField(OuterConfig, "alg B policy", optional=True)
-    algChoice = ChoiceField(str, "choice between algs A, B", default="A", allowed={"A":"choose algA", "B":"choose algB"})
+class Complex(Config):
+    c = ConfigField(InnerConfig, "an inner config", default=OuterConfig, optional=False)
+    r = RegistryField("restricted registry", typemap={"AAA":Simple, "BBB":InnerConfig}, restricted=True, default="AAA", optional=False)    
+    p = RegistryField("open, plugin-style registry", optional=True)
+    l = ConfigListField("list of configs", default=[Simple, InnerConfig, OuterConfig], optional=False, length=3)
 
-    def validate(self):
-        Config.validate(self);
-        if (self.algChoice == "A" and self.algA is None) or (self.algChoice == "B" and self.algB is None):
-            raise ValueError("no policy provided for chosen algorithm " + self.algChoice)
-
-class ListConfig(Config):
-    li = ListField(int, "list of ints", default = [1,2,3], optional=False, length=3, itemCheck=lambda x: x > 0)
-    
 class ConfigTest(unittest.TestCase):
-    def setUp(self):    
-        pass
+    def setUp(self): 
+        self.simple = Simple()
+        self.inner = InnerConfig()
+        self.outer = OuterConfig()
+        self.comp = Complex()
 
     def tearDown(self):
-        pass
+        del self.simple
+        del self.inner
+        del self.outer
+        del self.comp
 
     def testInit(self):
-        i = InnerConfig()
-        self.assertEqual(i.f, 0.0)
+        self.assertEqual(self.simple.i, None)
+        self.assertEqual(self.simple.f, 3.0)
+        self.assertEqual(self.simple.b, False)
+        self.assertEqual(self.simple.t, ("A", 1))
+        self.assertEqual(self.simple.c, "Hello")
+        self.assertEqual(list(self.simple.l), [1,2,3])
 
-        o = OuterConfig()
-        self.assertEqual(o.i.f, 5.0)
-        self.assertEqual(o.f, 0.0)
+        self.assertEqual(self.inner.f, 0.0)
 
-        p = PluginConfig()
-        self.assertEqual(p.algChoice, "A")
-        self.assertEqual(p.algA, None)
-        self.assertEqual(p.algB, None)
-   
+        self.assertEqual(self.outer.i.f, 5.0)
+        self.assertEqual(self.outer.f, 0.0)
+
+        self.assertEqual(self.comp.c.f, 0.0)
+        self.assertEqual(self.comp.r.active, "AAA")
+        self.assertEqual(self.comp.r[self.comp.r.active].f, 3.0)
+        self.assertEqual(self.comp.r["BBB"].f, 0.0)
+        self.assertEqual(self.comp.p.active, None)
+
+
     def testValidate(self):
-        o = OuterConfig()
-        o.i.f=-5
-        self.assertRaises(ValueError, o.validate)
-        o.i.f=10
-        o.validate()
+        self.simple.validate()
 
-        o.i = InnerConfig()
-        self.assertRaises(ValueError, o.validate)
+        self.inner.validate()
+        self.outer.i.f=-5
+        self.assertRaises(ValueError, self.outer.validate)
+        self.outer.i.f=10
+        self.outer.validate()
 
-        p = PluginConfig()
-        self.assertRaises(ValueError, p.validate)
-
-        p.algA = InnerConfig()
-        p.validate()
-
-        p = PluginConfig()
-        p.algB = OuterConfig()
-        p.algChoice = "B"
-        p.validate()
-
-        listConfig = ListConfig()
-        listConfig.validate()
-
-        listConfig.li[1]=5
-        self.assertEqual(listConfig.li[1], 5)
-
+        self.outer.i = InnerConfig
+        self.assertRaises(ValueError, self.outer.validate)
+        self.outer.i = InnerConfig()
+        self.assertRaises(ValueError, self.outer.validate)
+        
+        self.comp.validate()
+        self.comp.r= None
+        self.assertRaises(ValueError, self.comp.validate)
+        self.comp.r="BBB"
+        self.comp.validate()
 
     def testSave(self):
-        p = PluginConfig()
-        p.algB = OuterConfig()
-        p.algChoice = "B"
-        p.save("roundtrip.test")
+        self.comp.p["test"]=InnerConfig
+        self.comp.p["foo"]=Simple
+        self.comp.p = "test"
+
+        self.comp.save("roundtrip.test")
 
         roundTrip = Config.load("roundtrip.test")
         #os.remove("roundtrip.test")
 
-        self.assertEqual(p.algB.i.f, roundTrip.algB.i.f)
-        self.assertEqual(p.algA, roundTrip.algA)
-        self.assertEqual(p.algChoice, roundTrip.algChoice)
-
+        self.assertEqual(self.comp.c.f, roundTrip.c.f)
+        self.assertEqual(self.comp.r.active, roundTrip.r.active)
+        self.assertEqual(self.comp.p.active, roundTrip.p.active)
+        self.assertEqual(self.comp.p["foo"].i, roundTrip.p["foo"].i)
+        self.assertEqual(len(self.comp.l), len(roundTrip.l))
+        self.assertEqual(self.comp.l[0].f, roundTrip.l[0].f)
+        self.assertEqual(self.comp.l[1].f, roundTrip.l[1].f)
+        self.assertEqual(self.comp.l[2].f, roundTrip.l[2].f)
 
 def  suite():
     utilsTests.init()
