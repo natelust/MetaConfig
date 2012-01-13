472ce0f9168ae94d5f6228030f9c84be40175ec1
Martin Dubcovsky <dubcovsky@physics.ucdavis.edu>
2012-01-13 12:10:21 -0800

    Fixed some documentation Added convert.py to convert to Policy and PropertySet RegistryFields now support multi-selection

---
+++ python/lsst/pex/config/convert.py
@@ -0,0 +1,30 @@
+import lsst.pex.policy
+import lsst.daf.base
+
+def makePropertySet(config):
+    def _helper(dict_):
+        p = lsst.daf.base.PropertySet()
+        for k,v in dict_.iteritems():
+            if isinstance(v, dict):
+                p.set(k, _helper(v))
+            else:
+                p.set(k, v)
+        return p
+    if config:
+        return _helper(config.toDict())
+    else:
+        return None
+
+def makePolicy(config):
+    def _helper(dict_):
+        p = lsst.pex.policy.Policy()
+        for k,v in dict_.iteritems():
+            if isinstance(v, dict):
+                p.put(k, _helper(v))
+            else:
+                p.put(k, v)
+        return p
+    if config:
+        return _helper(config.toDict())
+    else:
+        return None
