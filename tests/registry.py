a4a4c6929b4613299a17ed72d17e8b4bf73da5c5
Russell Owen <rowen@uw.edu>
2012-01-24 15:31:33 -0600

    Use setUp and tearDown to make the tests more robust

--- tests/registry.py
+++ tests/registry.py
@@ -26,86 +26,109 @@ import unittest
 import lsst.utils.tests as utilsTests
 import lsst.pex.config as pexConfig
 
-class ParentConfig(pexConfig.Config):
-    pass
-
-configRegistry = pexConfig.makeConfigRegistry(doc="unit test configs", baseType=ParentConfig)
-algorithmRegistry = pexConfig.makeAlgorithmRegistry(doc="unit test algorithms", requiredAttributes=("foo",))
-
-@pexConfig.register("foo1", configRegistry) # one way to register
-class FooConfig1(ParentConfig):
-    pass
-
-class FooConfig2(ParentConfig):
-    pass
-configRegistry.add("foo2", FooConfig2)  # the other way to register
-
-class BarConfig(pexConfig.Config):
-    pass
-
-class Config1(pexConfig.Config):
-    pass
-
-class Config2(pexConfig.Config):
-    pass
-
-@pexConfig.register("foo1", algorithmRegistry)
-class FooAlg1(object):
-    ConfigClass = FooConfig1
-    def foo(self):
-        pass
-
-class FooAlg2(object):
-    ConfigClass = FooConfig2
-    def foo(self):
-        pass
-algorithmRegistry.add("foo2", FooAlg2)
-
-class BarAlg(object):
-    ConfigClass = BarConfig
-    def bar(self):
-        pass
-
 class ConfigTest(unittest.TestCase):
+    def setUp(self):
+        """Note: the classes are defined here in order to test the register decorator
+        """
+        class ParentConfig(pexConfig.Config):
+            pass
+        
+        self.configRegistry = pexConfig.makeConfigRegistry(doc="unit test configs", baseType=ParentConfig)
+        self.algorithmRegistry = pexConfig.makeAlgorithmRegistry(doc="unit test algorithms", requiredAttributes=("foo",))
+        
+        @pexConfig.register("foo1", self.configRegistry) # one way to register
+        class FooConfig1(ParentConfig):
+            pass
+        self.fooConfig1Class = FooConfig1
+        
+        class FooConfig2(ParentConfig):
+            pass
+        self.fooConfig2Class = FooConfig2
+        self.configRegistry.add("foo2", FooConfig2)  # the other way to register
+        
+        class Config1(pexConfig.Config):
+            pass
+        self.config1Class = Config1
+        
+        class Config2(pexConfig.Config):
+            pass
+        self.config2Class = Config2
+        
+        @pexConfig.register("foo1", self.algorithmRegistry)
+        class FooAlg1(object):
+            ConfigClass = FooConfig1
+            def foo(self):
+                pass
+        self.fooAlg1Class = FooAlg1
+        
+        class FooAlg2(object):
+            ConfigClass = FooConfig2
+            def foo(self):
+                pass
+        self.algorithmRegistry.add("foo2", FooAlg2)
+        self.fooAlg2Class = FooAlg2
+    
+    def tearDown(self):
+        del self.configRegistry
+        del self.algorithmRegistry
+        del self.fooConfig1Class
+        del self.fooConfig2Class
+        del self.fooAlg1Class
+        del self.fooAlg2Class
+        
     def testBasics(self):
-        self.assertEqual(configRegistry.get("foo1"), FooConfig1)
-        self.assertEqual(configRegistry.get("foo2"), FooConfig2)
-        self.assertEqual(set(configRegistry.keys()), set(("foo1", "foo2")))
-        self.assertEqual(algorithmRegistry.get("foo1"), FooAlg1)
-        self.assertEqual(algorithmRegistry.get("foo2"), FooAlg2)
-        self.assertEqual(set(algorithmRegistry.keys()), set(("foo1", "foo2")))
+        self.assertEqual(self.configRegistry.get("foo1"), self.fooConfig1Class)
+        self.assertEqual(self.configRegistry.get("foo2"), self.fooConfig2Class)
+        self.assertEqual(set(self.configRegistry.keys()), set(("foo1", "foo2")))
+        self.assertEqual(self.algorithmRegistry.get("foo1"), self.fooAlg1Class)
+        self.assertEqual(self.algorithmRegistry.get("foo2"), self.fooAlg2Class)
+        self.assertEqual(set(self.algorithmRegistry.keys()), set(("foo1", "foo2")))
 
     def testConfigReplace(self):
         """Test replacement in config registry
         """
-        self.assertRaises(Exception, configRegistry.add, "foo1", FooConfig1)
-        self.assertRaises(Exception, configRegistry.add, "foo3", FooConfig1, isNew=False)
-        self.assertEqual(configRegistry.get("foo1"), FooConfig1)
-        configRegistry.add("foo1", FooConfig2, isNew=False)
-        self.assertEqual(configRegistry.get("foo1"), FooConfig2)
-        configRegistry.add("foo1", FooConfig1, isNew=False)
-        self.assertEqual(configRegistry.get("foo1"), FooConfig1)
+        self.assertRaises(Exception, self.configRegistry.add, "foo1", self.fooConfig1Class)
+        self.assertRaises(Exception, self.configRegistry.add, "foo3", self.fooConfig1Class, isNew=False)
+        self.assertEqual(self.configRegistry.get("foo1"), self.fooConfig1Class)
+        self.configRegistry.add("foo1", self.fooConfig2Class, isNew=False)
+        self.assertEqual(self.configRegistry.get("foo1"), self.fooConfig2Class)
+        self.configRegistry.add("foo1", self.fooConfig1Class, isNew=False)
+        self.assertEqual(self.configRegistry.get("foo1"), self.fooConfig1Class)
     
     def testAlgorithmReplace(self):
         """Test replacement in algorithm registry
         """
-        self.assertRaises(Exception, algorithmRegistry.add, "foo1", FooAlg1)
-        self.assertRaises(Exception, algorithmRegistry.add, "foo3", FooAlg1, isNew=False)
-        self.assertEqual(algorithmRegistry.get("foo1"), FooAlg1)
-        algorithmRegistry.add("foo1", FooAlg2, isNew=False)
-        self.assertEqual(algorithmRegistry.get("foo1"), FooAlg2)
-        algorithmRegistry.add("foo1", FooAlg1, isNew=False)
-        self.assertEqual(algorithmRegistry.get("foo1"), FooAlg1)
+        self.assertRaises(Exception, self.algorithmRegistry.add, "foo1", self.fooAlg1Class)
+        self.assertRaises(Exception, self.algorithmRegistry.add, "foo3", self.fooAlg1Class, isNew=False)
+        self.assertEqual(self.algorithmRegistry.get("foo1"), self.fooAlg1Class)
+        self.algorithmRegistry.add("foo1", self.fooAlg2Class, isNew=False)
+        self.assertEqual(self.algorithmRegistry.get("foo1"), self.fooAlg2Class)
+        self.algorithmRegistry.add("foo1", self.fooAlg1Class, isNew=False)
+        self.assertEqual(self.algorithmRegistry.get("foo1"), self.fooAlg1Class)
     
     def testClass(self):
         """Test subclass test of ConfigRegistry
         """
-        self.assertRaises(Exception, configRegistry.add, "bar", BarConfig)
+        class BarConfig(pexConfig.Config):
+            pass
+        self.assertRaises(Exception, self.configRegistry.add, "bar", BarConfig)
     
     def testAttr(self):
         """Test attribute test of AlgorithmRegistry
         """
-        self.assertRaises(Exception, algorithmRegistry.add, "bar", BarAlg)
+        class FooAlgNoConfig(object):
+            def foo(self):
+                pass
+        self.assertRaises(Exception, self.algorithmRegistry.add, "fooNoConfig", FooAlgNoConfig)
+
+        class BarConfig(pexConfig.Config):
+            pass
+        class BarAlg(object):
+            ConfigClass = BarConfig
+            def bar(self):
+                pass
+
+        self.assertRaises(Exception, self.algorithmRegistry.add, "bar", BarAlg)
 
 def  suite():
     utilsTests.init()
