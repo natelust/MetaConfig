3f9e77fe9e04066d86e2622f003c173e366f4207
Serge Monkewitz <smm@ipac.caltech.edu>
2012-05-03 19:42:40 -0500

    lsst.pex.config.List was missing __ne__ method, breaking Config comparisons

--- python/lsst/pex/config/listField.py
+++ python/lsst/pex/config/listField.py
@@ -85,6 +85,9 @@ class List(collections.MutableSequence):
         except:
             False
 
+    def __ne__(self, other):
+        return not self.__eq__(other)
+
 
 class ListField(Field):
     """
