140353da3023b411701ebcc7d7f29fb03db61a5a
Russell Owen <rowen@uw.edu>
2018-08-14 16:03:23 -0700

    Remove `from __future__ import`

--- tests/test_ticket1911.py
+++ tests/test_ticket1911.py
@@ -21,7 +21,6 @@
 # the GNU General Public License along with this program.  If not,
 # see <http://www.lsstcorp.org/LegalNotices/>.
 #
-from __future__ import print_function
 from builtins import object
 import unittest
 import lsst.utils.tests
